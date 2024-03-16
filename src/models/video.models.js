7226e6c206015028a77f5368d37e17a0665aefd4
m-kazi <kazzcode@gmail.com>
2024-03-17 00:32:53 +0600

    initial user & video models

---
+++ src/models/video.models.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Nuber,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
