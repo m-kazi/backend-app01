7bda775e943e01b23d6c2794a4a23115ff2feed7
m-kazi <kazzcode@gmail.com>
2024-03-19 22:11:50 +0600

    Added user & video model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,8 +1,10 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
       unique: true,
@@ -17,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -44,5 +46,50 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+/* Best practice to write mongoose middleware pre hook,
+   here we are using 'pre' hook to encrypt password before saving it into the DB */
+userSchema.pre("save", async function (next) {
+  // Only if password field is modified, run the encrypt otherwise nope.
+  if (this.isModified("password")) {
+    // bcrypt syntax to encrypt 'password' field
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+
+// custom method to check if the password is matching or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// to generate JWT access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// to generate JWT refresh token, using only id coz it refreshes
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
